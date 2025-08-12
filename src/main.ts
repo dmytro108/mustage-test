3b06b4a5b53087e285f55ba3b2f654f247932702
Dmitry Fedunets <88787749+dmytro108@users.noreply.github.com>
2025-08-12 18:13:58 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	// Enable graceful shutdown
+	//some update to test PR
 	app.enableShutdownHooks();
 
 	await app.listen(process.env.PORT ?? 3000);
